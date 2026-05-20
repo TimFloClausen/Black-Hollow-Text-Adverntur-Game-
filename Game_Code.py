1b2199e8af2a58b6dfc8b0994c263166227cc52d
Tim Clausen <timclausen1004@gmail.com>
2026-05-20 22:24:43 +0200

    New Story Part
    I added new story parts. Bugs and more logic for the game I will fix/add soone.

--- Game_Code.py
+++ Game_Code.py
@@ -1,15 +1,15 @@
-print("Welcome to Black Hollows!")
+print("Welcome to Black Hollow!")
 print("A Text Adventure made by TimFloClausen")
 input("Press Enter to continue... ")
 print("""
 ══════════════════════════════════════
-        BLACK HOLLOWS - INTRO
+        BLACK HOLLOW - INTRO
 ══════════════════════════════════════
-Welcome to Black Hollows.
+Welcome to Black Hollow.
 A small town in the north of the United Kingdom.
 Run-down, remote and rainy, surrounded by forests
 and old stone houses.
-Black Hollows has:
+Black Hollow has:
 - an old elementary school, built after World War II
 - abandoned bunkers from World War II
 - an old atomic bunker renewed during the Cold War
@@ -18,7 +18,7 @@ Black Hollows has:
 - a dilapidated cinema
 - a gas station called "Gips"
 You are Thomas Bruce.
-Black Hollows is your hometown, and you are the
+Black Hollow is your hometown, and you are the
 owner of "Gips".
 You inherited the gas station from your father,
 who died of cancer last year.
@@ -45,7 +45,6 @@ elif task_1 == "pills":
     print("Two police officers are still waiting outside.")
 else:
     print("I didn't understand that. Please choose 'door' or 'pills'.")
-    exit()
 
 input("""
 
@@ -53,14 +52,14 @@ Mr Potter, the local police officer, and his partner are standing in front of yo
 "Good morning, Mr Bruce. Sorry for the disturbance at this early hour.
 We need to inform you that your brother is dead.
 A woman found his body in the forest near the old atomic bunker. Can we come in and ask you some questions?"
-""" )
+""")
     
 print("""
 
 You are shocked. Your beloved brother James seems to be dead.
 You haven't seen him since the burial of your father.
 He left the town and went to Birmingham, like you wanted to do as well...
-But why was he in Black Hollows?
+But why was he in Black Hollow?
 And why did he die?
 And how?
 So many questions are going through your head.
@@ -68,10 +67,43 @@ So many questions are going through your head.
 
 input("""
 
-You let the bote officer in your arpartment. You point on the chairs in the kitchen an sadi that the bothe officer can take place.
-Two small wooden chairs,
+You let the police officer into your apartment. You point to the chairs in the kitchen and say that the both officers can take a seat.
+Two small wooden chairs.
 Your initials are carved into the backrest: T + J.
-The paint is faded, but still visible. You and your Brotehr painted when you were children. 
- """ )
+The paint is faded, but still visible. You and your brother painted them when you were children.
+""")
 
-print("")
+print("""The police officer asks you some questions. You can't remember much after you answer them.
+      After half an hour they are finished.
+      Potter looks at you seriously in the eyes and asks: When we found the body of James,
+        we saw something really strange. On his body was a photo of him, I could show you,
+      but I want to tell you that it's really disturbing and it affects people in general if they see
+      the dead body of their relatives or friends...
+      So do you want to see the photo or just have it described?""")
+
+task_2 = input("Do you want to see a picture or only a description? (photo/description) ")
+
+if task_2 == "photo":
+    print("""Potter silently hands you the photo.
+At first you don't understand what you're seeing.
+Then you realize it is James.
+His skin has a strange color.
+Not pale.
+Not rotten.
+Green.
+A deep, unnatural green.
+Like something inside his body had changed.""")
+
+elif task_2 == "description":
+     print("""Potter beginn to speak:
+At first we didn't understand what we was seeing.
+Then we realize it was James.
+His skin had a strange color.
+Not pale.
+Not rotten.
+Green.
+A deep, unnatural green.
+Like something inside his body had changed.""")
+    
+else:
+    print("I didn't understand that. Please choose 'door' or 'pills'.")
