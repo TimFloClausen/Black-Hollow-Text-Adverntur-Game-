278c9797e95675eac74b7aff762b5f9d8e7f18e2
Tim Clausen <timclausen1004@gmail.com>
2026-05-22 19:12:20 +0200

    Externalize story text into story_txt files
    Move all story text out of Game_Code.py into separate files under story_txt and update the game to load them at runtime. Added story_txt/{intro,knock,door_police,chairs,shocked,photo_ordescription,photo,description}.txt and changed Game_Code.py to use pathlib (BASE_PATH/STORY_PATH) and open these UTF-8 text files, replacing large inline multiline strings. Also adjusted some input prompts and choice output to print the loaded text and small UI messaging tweaks.

--- Game_Code.py
+++ Game_Code.py
@@ -1,4 +1,52 @@
 import time
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+STORY_PATH = BASE_PATH / "story_txt"
+
+with open(STORY_PATH / "intro.txt", "r", encoding="utf-8") as file:
+    intro = file.read()
+
+with open(STORY_PATH / "knock.txt", "r", encoding="utf-8") as file:
+    knock = file.read()
+
+with open("story_txt/door_police.txt", "r", encoding="utf-8") as file:
+    door_police = file.read()
+
+with open("story_txt/chairs.txt", "r", encoding="utf-8") as file:
+    chairs = file.read()
+
+with open("story_txt/shocked.txt", "r", encoding="utf-8") as file:
+    shocked = file.read()
+
+with open("story_txt/photo_ordescription.txt", "r", encoding="utf-8") as file:
+    photo_ordescription = file.read()
+
+with open("story_txt/photo.txt", "r", encoding="utf-8") as file:
+    photo  = file.read()
+
+with open("story_txt/description.txt", "r", encoding="utf-8") as file:
+    description  = file.read()
+
+
+
+
+
+
+    
+
+
+    
+    
+
+
+
+
+
+
+
+    
+
 
 def wait():
     input()
@@ -7,36 +55,10 @@ def wait():
 
 print("Welcome to Black Hollow!")
 print("A Text Adventure made by TimFloClausen")
-input("Press enter to the game: ")
-print("""
-══════════════════════════════════════
-        BLACK HOLLOW - INTRO
-══════════════════════════════════════
-Welcome to Black Hollow.
-A small town in the north of the United Kingdom.
-Run-down, remote and rainy, surrounded by forests
-and old stone houses.
-Black Hollow has:
-- an old elementary school, built after World War II
-- abandoned bunkers from World War II
-- an old atomic bunker renewed during the Cold War
-- a grocery store
-- four pubs
-- a dilapidated cinema
-- a gas station called "Gips"
-You are Thomas Bruce.
-Black Hollow is your hometown, and you are the
-owner of "Gips".
-You inherited the gas station from your father,
-who died of cancer last year.
-You once wanted to leave this town and move to
-Birmingham.
-But you could never bring yourself to close the
-gas station.
-So...
-you stayed.
-""")
-input("Press Enter to continou! ")
+input("Press enter to start the game: ")
+print(intro)
+
+input("Press Enter to continue! ")
 
 print("Knock...")
 time.sleep(1)
@@ -47,12 +69,7 @@ time.sleep(1)
 print("Knock...") 
 time.sleep(1)
 
-print("""You wake up. Your head is pounding,
-It feels like tiny creatures inside your skull are hitting it with hammers.
-You know why you have such pain: a big hangover from last night at the pub.
-Someone is knocking at the door.
-The bell is broken, so the person has to knock on the old red door of the apartment you inherited from your father.
-""")
+print(knock)
 
 wait()
 
@@ -78,49 +95,21 @@ elif task_1 == "pills":
 
     
 
-print("""
-
-Mr Potter, the local police officer, and his partner are standing in front of your door.
-"Good morning, Mr Bruce. Sorry for the disturbance at this early hour.
-We need to inform you that your brother is dead.
-A woman found his body in the forest near the old atomic bunker. Can we come in and ask you some questions?
-""")
+print(door_police)
 
 wait() 
     
-print("""
-
-You are shocked. Your beloved brother James seems to be dead.
-You haven't seen him since the burial of your father.
-He left the town and went to Birmingham, like you wanted to do as well...
-But why was he in Black Hollow?
-And why did he die?
-And how?
-So many questions are going through your head.
-""")
-
+print(shocked)
 
 wait()
 
 
-print("""
-
-You let the police officer into your apartment. You point to the chairs in the kitchen and say that the both officers can take a seat.
-Two small wooden chairs.
-Your initials are carved into the backrest: T + J.
-The paint is faded, but still visible. You and your brother painted them when you were children.
-""")
+print(chairs)
 
 
 wait()
 
-print("""The police officer asks you some questions. You can't remember much after you answer them.
-      After half an hour they are finished.
-      Potter looks at you seriously in the eyes and asks: When we found the body of James,
-        we saw something really strange. On his body was a photo of him, I could show you,
-      but I want to tell you that it's really disturbing and it affects people in general if they see
-      the dead body of their relatives or friends...
-      So do you want to see the photo or just have it described?""")
+print(photo_ordescription)
 
 wait()
 
@@ -137,24 +126,11 @@ while task_2 not in ["photo", "description"]:
 
 
 if task_2 == "photo":
-    print("""Potter silently hands you the photo.
-At first you don't understand what you're seeing.
-Then you realize it is James.
-His skin has a strange color.
-Not pale.
-Not rotten.
-Green.
-A deep, unnatural green.
-Like something inside his body had changed.""")
+    print("You chose photo!")
+    print(photo)
 
 elif task_2 == "description":
-     print("""Potter beginn to speak:
-At first we didn't understand what we was seeing.
-Then we realize it was James.
-His skin had a strange color.
-Not pale.
-Not rotten.
-Green.
-A deep, unnatural green.
-Like something inside his body had changed.""")
+     print("You chose description!")
+     print(description)
+
     
